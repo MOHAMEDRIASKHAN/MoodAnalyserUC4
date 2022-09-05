826fb1c271923456baaf68460e83fdd0a041ce1e
MOHAMEDRIASKHAN <mohamedriaskhan@gmail.com>
2022-09-05 07:58:51 -0700

    IncorrectMoodException.cs

---
+++ IncorrectMoodException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserUC4
+{
+    public class IncorrectMoodException
+    {
+        public enum ExceptionType
+        {
+            ENTERD_NULL, ENTERED_EMPTY,
+            NO_SUCH_CLASS, NO_SUCH_METHOD
+        }
+
+        public ExceptionType type;
+        public IncorrectMoodException(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+
+    }
